30a803cb5585a4c119e2ea2eb028a00e1a2dc0d4
miaosuqi <miaosuqi@baidu.com>
2023-08-07 15:54:39 +0800

    tube 2

--- src/dfs/tube/Main.java
+++ src/dfs/tube/Main.java
@@ -5,16 +5,29 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+//        int[][] tubesValue = new int[][]{
+//                {1, 2, 1, 2},
+//                {2, 1, 2, 1},
+//                {0, 0, 0, 0}
+//        };
         int[][] tubesValue = new int[][]{
-                {1, 2, 1, 2},
-                {2, 1, 2, 1},
+                {1, 2, 2, 3},
+                {3, 1, 4, 5},
+                {6, 5, 4, 7},
+                {7, 8, 8, 3},
+                {6, 1, 8, 4},
+                {8, 3, 1, 6},
+                {9, 9, 5, 4},
+                {5, 7, 6, 9},
+                {2, 7, 2, 9},
+                {0, 0, 0, 0},
                 {0, 0, 0, 0}
         };
         Solution solution = new Solution();
         ArrayList<LinkedList<Integer>> tubes = solution.transform(tubesValue);
         solution.dfs(tubes);
         for (Integer[] integers : solution.result) {
-            System.out.printf("%d -> %d", integers[0], integers[1]);
+            System.out.printf("%d -> %d\n", integers[0] + 1, integers[1] + 1);
         }
     }
 }
--- src/dfs/tube/Solution.java
+++ src/dfs/tube/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static final int elementNum = 4;
 
     // 结果 即操作步骤 [0] -> [1]
-    public ArrayList<Integer[]> result;
+    public LinkedList<Integer[]> result = new LinkedList<>();
 
     // 检查是否为完成状态
     public boolean checkFinish(ArrayList<LinkedList<Integer>> tubes) {
@@ -93,12 +93,58 @@ public class Solution {
         return set.size();
     }
 
-    public void aTob(LinkedList<Integer> a, LinkedList<Integer> b) {
+    // a向b倒入
+    public int aTob(LinkedList<Integer> a, LinkedList<Integer> b) {
+        if (a.size() == 0) {
+            return 0;
+        }
+        int changeVal = 0;
+        Integer topColor = a.getFirst();
+        while (a.size() > 0 && a.getFirst().equals(topColor)) {
+            b.addFirst(a.getFirst());
+            a.removeFirst();
+            changeVal++;
+        }
+        return changeVal;
+    }
 
+    // 用于回溯失败后恢复，由于此时顶层颜色不同，强制倒回
+    public void recover(LinkedList<Integer> a, LinkedList<Integer> b, int changeVal) {
+        for (int i = 0; i < changeVal; i++) {
+            b.addFirst(a.getFirst());
+            a.removeFirst();
+        }
     }
 
     public void dfs(ArrayList<LinkedList<Integer>> tubes) {
-
+        if (checkFinish(tubes)) {
+            return;
+        }
+        ArrayList<Integer[]> canExchange = new ArrayList<>();
+        for (int i = 0; i < tubes.size() - 1; i++) {
+            for (int j = i + 1; j < tubes.size(); j++) {
+                if (getOperation(tubes.get(i), tubes.get(j)) == 1) {
+                    canExchange.add(new Integer[]{i, j});
+                }
+                if (getOperation(tubes.get(i), tubes.get(j)) == 2) {
+                    canExchange.add(new Integer[]{j, i});
+                }
+                if (getOperation(tubes.get(i), tubes.get(j)) == 3) {
+                    canExchange.add(new Integer[]{i, j});
+                    canExchange.add(new Integer[]{j, i});
+                }
+            }
+        }
+        for (Integer[] integers : canExchange) {
+            result.addLast(integers);
+            int changeVal = aTob(tubes.get(integers[0]), tubes.get(integers[1]));
+            dfs(tubes);
+            if (checkFinish(tubes)) {
+                return;
+            }
+            recover(tubes.get(integers[1]), tubes.get(integers[0]), changeVal);
+            result.removeLast();
+        }
     }
 
     public ArrayList<LinkedList<Integer>> transform(int[][] tubesValue) {
