d5476fb628fc932c54eb8cc0e7eb9596f86892c6
miaosuqi <miaosuqi@baidu.com>
2023-08-21 13:59:29 +0800

    code141 判断有环链表

---
+++ src/linkedList/code141/ListNode.java
@@ -0,0 +1,10 @@
+package linkedList.code141;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/linkedList/code141/Solution.java
@@ -0,0 +1,20 @@
+package linkedList.code141;
+
+public class Solution {
+    // code141 判断有环链表
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
